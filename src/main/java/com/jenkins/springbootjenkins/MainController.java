a237f00e7a8d81c1beba5e0588fa92078904895d
yilei111 <2714512416@qq.com>
2020-09-13 21:06:47 +0800

    初始化提交代码

---
+++ src/main/java/com/jenkins/springbootjenkins/MainController.java
@@ -0,0 +1,22 @@
+package com.jenkins.springbootjenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yilei
+ * @version 1.0
+ * @description TODO
+ * @className MainController
+ * @date 2020/9/13 16:33
+ **/
+@RestController
+@RequestMapping("/main")
+public class MainController {
+
+    @GetMapping(value = "/index")
+    public String send() {
+        return "访问成功！";
+    }
+}
---
+++ src/test/java/com/jenkins/springbootjenkins/SpringbootJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.springbootjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
